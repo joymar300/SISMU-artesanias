fbd92ea6bd41078b3c45d796eec453adfb3c9fd6
joymar300 <joymar300@hotmail.com>
2022-04-27 15:20:05 -0500

    issue 24 and 30
    i created the controller to model productos an category and link categories with productos

---
+++ rails/SISMU/app/controllers/producto_controller.rb
@@ -0,0 +1,48 @@
+class ProductoController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @productos = Producto.all
+  end
+  def show
+    @producto = Producto.find(params[:id])
+  end
+  
+  def new
+    @producto = Producto.new
+  end
+
+  def create
+    @producto = Producto.new(producto_params)
+    if @producto.save 
+      redirect_to productos_path
+    else
+      render :new
+    end
+  end
+  
+  def edit
+    producto = Producto.find(params[:id])
+  end
+
+
+  def update
+    @producto = Producto.find(params[:id])
+    if @producto.update(producto_params)
+      redirect_to productos_path
+    else
+        render :edit
+    end
+  end
+
+  def destroy
+    @producto = Producto.find(params[:id]).destroy
+    redirect_to productos_path
+
+  end
+
+  private
+
+    def producto_params
+      params.require(:producto).permit(:nombre)
+    end
+end
--- rails/SISMU/app/models/producto.rb
+++ rails/SISMU/app/models/producto.rb
@@ -1,3 +1,3 @@
 class Producto < ApplicationRecord
-    
+    has_and_belongs_to_many :categories
 end
