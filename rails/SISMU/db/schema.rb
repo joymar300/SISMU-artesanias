616612034d5430a0af3195e95a96a89e386c4c2e
gustavo <valdezgustavo461@gmail.com>
2022-05-02 23:24:41 -0500

    modificacion del registrar de devise

--- rails/SISMU/db/schema.rb
+++ rails/SISMU/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_27_195808) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_03_040619) do
   create_table "categories", force: :cascade do |t|
     t.string "tipo"
     t.datetime "created_at", null: false
@@ -51,6 +51,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_27_195808) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nombre"
+    t.string "apellido"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
