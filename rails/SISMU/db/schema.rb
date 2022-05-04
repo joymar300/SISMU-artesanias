dce98e1d719c67094446238ab1a1bdac95dce992
joymar300 <74611985+joymar300@users.noreply.github.com>
2022-05-04 17:27:49 -0500

    crear guardado de imagen de los productos

--- rails/SISMU/db/schema.rb
+++ rails/SISMU/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_03_040619) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_04_220218) do
   create_table "categories", force: :cascade do |t|
     t.string "tipo"
     t.datetime "created_at", null: false
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_03_040619) do
     t.text "imagen_pro"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar"
   end
 
   create_table "roles", force: :cascade do |t|
