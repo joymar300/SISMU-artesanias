7bb1fba312f439e7911656bab15583755be6e606
gustavo <valdezgustavo461@gmail.com>
2022-05-04 08:26:51 -0500

    modificado el devise en el registrar

--- rails/SISMU/db/migrate/20220503040619_add_primer_nombre_to_users.rb
+++ rails/SISMU/db/migrate/20220503040619_add_primer_nombre_to_users.rb
@@ -1,8 +1,10 @@
 class AddPrimerNombreToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :nombre, :string
+    add_column :users, :primernombre, :string
+    add_column :users, :segundonombre, :string
+    add_column :users, :primerapellido, :string
+    add_column :users, :segundoapellido, :string
     
-    add_column :users, :apellido, :string
- 
+    
   end
 end
