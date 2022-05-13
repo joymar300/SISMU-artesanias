5f20a4d1725e59e6ef2718d9759eaf89a796135d
joymar300 <joymar300@hotmail.com>
2022-05-12 23:17:39 -0500

    agregando tabla categories y relacion

--- rails/SISMU/app/controllers/productos_controller.rb
+++ rails/SISMU/app/controllers/productos_controller.rb
@@ -44,6 +44,6 @@ class ProductosController < ApplicationController
   private
 
     def producto_params
-      params.require(:producto).permit(:nombre_pro, :precio_pro, :referencia_pro)
+      params.require(:producto).permit(:nombre_pro, :precio_pro, :referencia_pro, category_ids:[])
     end
 end
