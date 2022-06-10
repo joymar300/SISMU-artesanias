a62801b633c7d73fc69202fc928a27e11ec648ba
gustavo <valdezgustavo461@gmail.com>
2022-06-10 16:19:33 -0500

    validaciones realizadas en los modelos que no tieennen asociacion

--- rails/SISMU/app/models/artesano.rb
+++ rails/SISMU/app/models/artesano.rb
@@ -1,11 +1,16 @@
 class Artesano < ApplicationRecord
+    validates :nombre, presence: true
+    validates :nombre,:snombre,:nombre2_cli,:sapellido,:cedula, :tel ,presence: {:message => "requeridos"}
+   
     has_many :dproductions, dependent: :destroy
     has_many :production, through: :dproductions
-
+   
     def nombre_completo
         "#{cedula}- #{nombre} #{apellido}"
     end
     def nombre_apellido
         "#{nombre} #{apellido}"
     end
+   
+
 end
--- rails/SISMU/app/models/empresa.rb
+++ rails/SISMU/app/models/empresa.rb
@@ -1,5 +1,8 @@
 class Empresa < ApplicationRecord
     has_many :emfacturas, dependent: :destroy
+    validates :id, :nombre, :direccion, :tel, presence: {:message => "requeridos"}
+
+    validates :id, uniqueness: {:message => "nit unico"}
     def empresa_busqueda
         "#{id}- #{nombre}"
     end
