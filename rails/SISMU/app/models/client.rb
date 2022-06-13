f8d018aed7861c4f2de3833931a42ae133775a57
joymar300 <joymar300@hotmail.com>
2022-06-13 17:10:20 -0500

    cambios id

--- rails/SISMU/app/models/client.rb
+++ rails/SISMU/app/models/client.rb
@@ -5,8 +5,6 @@ class Client < ApplicationRecord
     def nombre_completo
         "#{id}- #{nombre_cli} #{nombre2_cli}"
     end
-    validates :cedula_cli, :correo_cli, uniqueness: {:message => "campos unico"}
-    validates :cedula_cli, :nombre_cli, :nombre2_cli,:apellido_cli,:apellido2_cli,
-     :tel_cli, :correo_cli, :direccion_cli ,presence: {:message => "requeridos"}
-     validates :id, uniqueness: {:message => "esa cedula está en uso"}
+    validates :id, :correo_cli, uniqueness: {:message => "campos unico"}
+    validates :id, :nombre_cli, :nombre2_cli,:apellido_cli,:apellido2_cli, :tel_cli, :correo_cli, :direccion_cli ,presence: {:message => "requeridos"}
 end
