e83363041843f95585959ed18ac13e7c7753b296
gustavo <valdezgustavo461@gmail.com>
2022-06-22 10:45:59 -0500

    agregado datos basicos del usuario

--- rails/SISMU/app/controllers/application_controller.rb
+++ rails/SISMU/app/controllers/application_controller.rb
@@ -13,8 +13,8 @@ class ApplicationController < ActionController::Base
         protected
         
         def configure_permitted_parameters
-          devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar, :primernombre, :segundonombre, :primerapellido , :segundoapellido, :email, :password])
-          devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
-          
+          devise_parameter_sanitizer.permit(:sign_up, keys: [:primernombre, :segundonombre, :primerapellido , :segundoapellido, :email, :password,:avatar])
+          devise_parameter_sanitizer.permit(:account_update, keys: [:primernombre, :segundonombre, :primerapellido , :segundoapellido, :email, :password,:avatar])
+        # evise_parameter_sanitizer.permit(:sign_up) { |u| u.permit() }  d
         end
 end
