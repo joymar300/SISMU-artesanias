f0676402003c0d7c4cb75e5a00675be8c697b865
gustavo <valdezgustavo461@gmail.com>
2022-06-22 09:12:29 -0500

    sale foto del perfil

--- rails/SISMU/app/models/user.rb
+++ rails/SISMU/app/models/user.rb
@@ -4,7 +4,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         has_one_attached :avatar
 
+         def avatar_thumbnail
+           if avatar.attached?
+            # avatar.variant(resize: "150x150").processed
+            avatar.variant(resize_to_fill: [150, nil])
+           else
+             '/default_profile.jpg'
+           end
+         end
 has_one :profile, dependent: :destroy
 
 after_create :set_profile
