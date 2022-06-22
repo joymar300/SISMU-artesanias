63dfe18a2a29575f7b08685c146bd9a78b23d1e2
gustavo <valdezgustavo461@gmail.com>
2022-06-22 17:04:10 -0500

    borrado de una duplicacion de active migrate y colocado los datos basicos del uruario

--- rails/SISMU/app/controllers/application_controller.rb
+++ rails/SISMU/app/controllers/application_controller.rb
@@ -15,8 +15,8 @@ class ApplicationController < ActionController::Base
         def configure_permitted_parameters
 
             
-            devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar])
-            devise_parameter_sanitizer.permit(:account_update, keys: [:avatar]) 
+            devise_parameter_sanitizer.permit(:sign_up, keys: [ :primernombre,:segundonombre,:primerapellido,:segundoapellido, :avatar])
+            devise_parameter_sanitizer.permit(:account_update, keys: [:primernombre,:segundonombre,:primerapellido,:segundoapellido,:avatar]) 
 
          
 
