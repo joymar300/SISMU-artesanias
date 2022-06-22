1d65c26ccef37120dde458eb9ddf4c922e61cda9
gustavo <valdezgustavo461@gmail.com>
2022-06-22 16:41:58 -0500

    agregado correctamente del perfil con imagen por defecto

--- rails/SISMU/app/controllers/application_controller.rb
+++ rails/SISMU/app/controllers/application_controller.rb
@@ -6,6 +6,8 @@ class ApplicationController < ActionController::Base
     protected
 
         def configure_permitted_parameters
-            devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:primernombre, :segundonombre, :primerapellido , :segundoapellido, :email, :password) }
+            
+            devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar])
+            devise_parameter_sanitizer.permit(:account_update, keys: [:avatar]) 
         end
 end
--- rails/SISMU/app/models/user.rb
+++ rails/SISMU/app/models/user.rb
@@ -4,12 +4,30 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+has_one_attached :avatar
 
-has_one :profile, dependent: :destroy
-
-after_create :set_profile
-
-def set_profile
-  self.profile = Profile.create()
+after_commit :add_default_avatar, on: %i[create update]
+def avatar_thumbnail
+  if avatar.attached?
+  avatar.variant(resize_to_fill: [150, nil])
+  else
+"/default_profile.jpg"
+  end
 end
+
+private 
+def   add_default_avatar
+  unless avatar.attached?
+    avatar.attach(
+      io: File.open(
+        Rails.root.join(
+          'app', 'assets', 'images', 'default_profile.jpg'
+        )
+      ),
+      filename: 'defaul_profile.jpg',
+      content_type: 'image/jpg'
+    )
+    
+  end
+  end
 end
