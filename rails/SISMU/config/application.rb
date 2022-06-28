911e3f4013172d67ca377fdeee12fbcfb71a756b
David Garcia <davidjosegarcia12@gmail.com>
2022-06-28 15:20:21 -0500

    cambios

--- rails/SISMU/config/application.rb
+++ rails/SISMU/config/application.rb
@@ -10,7 +10,7 @@ module SISMU
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    config.i18n.default_locale = :es
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -19,4 +19,5 @@ module SISMU
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
   end
+ 
 end
