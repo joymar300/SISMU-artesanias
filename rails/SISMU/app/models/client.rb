044f30df2fd6754b109061d7c13caa54db9dbccf
joymar300 <74611985+joymar300@users.noreply.github.com>
2022-06-30 02:18:42 -0500

    correción de validaciones
    corregí los escritos de las validaciones

--- rails/SISMU/app/models/client.rb
+++ rails/SISMU/app/models/client.rb
@@ -5,7 +5,8 @@ class Client < ApplicationRecord
     def nombre_completo
         "#{id}- #{nombre_cli} #{nombre2_cli}"
     end
-    validates :id, :correo_cli, uniqueness: {:message => "Campo único"}
+    validates :id, uniqueness: {:message => "Este numero de cedula ya se encuentra en uso"}
+    validates :id, :correo_cli, uniqueness: {:message => "El correoingresado ya se encuentran en uso"}
     validates :id, :nombre_cli, :apellido_cli, :tel_cli, :correo_cli, :direccion_cli, presence: {:message => "Requerido"}
     validates :nombre_cli, :apellido_cli, format: { with: /\A[a-zA-Z]+\z/, message: "Solo se permiten letras" }
     validates :id, length: {  in:7.. 10, :message=>"Tiene que contener mínimo 7 numeros maximo 10" }
