64929cf20b26cca81879f941f2bfe4a590f86893
joymar300 <74611985+joymar300@users.noreply.github.com>
2022-08-29 23:48:43 -0500

    a

--- rails/SISMU/app/models/client.rb
+++ rails/SISMU/app/models/client.rb
@@ -10,16 +10,15 @@ class Client < ApplicationRecord
     end
     validates :id, uniqueness: {  :message => "«Este número de identificación  ya se encuentra en uso»" }
     validates :id, :correo_cli, uniqueness: { :message => "«El correo electronico ingresado ya se encuentra en uso»" }
-    validates :id, :nombre_cli, :apellido_cli, :tel_cli, :correo_cli, :direccion_cli,:ciudad, presence: { :message => "«Este campo es obligatorio»"  }
-    validates :nombre_cli, :apellido_cli, :ciudad, format: { with: /\A[a-zA-ZÀ-ÿ]+\z/, :message => "«Se permiten solo letras» (no se permiten carácteres especiales o espacios)" }
+    validates :id, :nombre_cli, :apellido_cli, :tel_cli, :direccion_cli, presence: { :message => "«Este campo es obligatorio»"  }
+    validates :nombre_cli, :apellido_cli, format: { with: /\A[a-zA-ZÀ-ÿ]+\z/, :message => "«Se permiten solo letras» (no se permiten carácteres especiales o espacios)" }
     validates :id, length: {  in:7.. 10, :message=>"«Debe contener entre 7 y 10 digitos numéricos»" }
 
     # nuevas validaciones actualizacion version 1.2
     
     validates :tel_cli, length: { is: 10, message: "«Introduzca un numero de telefono válido» (El número de telefono solo puede tener 10 digitos)" }
     validates :id, :tel_cli, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 99999999999, :message => "«No se permiten números negativos»"  }
-    validates :correo_cli, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i, :message => "«Introduzca una dirección de correo electrónico válida»"  }
-    validates :barrio,  format: { with: /\A[a-zA-ZÀ-ÿ\s]+$\z/, :message => "«Se permiten solo letras» (no se permiten carácteres especiales)" }
+   
 
     
     def self.search(search)
